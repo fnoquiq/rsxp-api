76a2af17ad04cfcf7dbd90c3e56c6f3f92b21708
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2020-03-08 22:23:43 -0300

    :rocket: Adiciona validações para Session, Forgot e Reset

--- start/app.js
+++ start/app.js
@@ -18,6 +18,7 @@ const providers = [
   '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/framework/providers/ViewProvider',
   '@adonisjs/mail/providers/MailProvider',
+  '@adonisjs/validator/providers/ValidatorProvider',
 ];
 
 /*
